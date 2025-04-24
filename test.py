a5ce7d461d8f5af647db112a110f406feed404b2
Arjithar8 <arjithar8@gmail.com>
2025-04-25 00:21:15 +0530

    made the test wrong

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ import requests
 class Test:
     def test(self):
         req = requests.get("http://localhost:8000/health")
-        assert req.json()=={ "status": "OK" }
+        assert req.json()=={ "status": "oK" }
